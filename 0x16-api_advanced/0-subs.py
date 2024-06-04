1a4d30da17944fc0d029e4e107281a3943db5ad9
nabil mkadmi <nabilmkadmi@gmail.com>
2024-06-04 16:52:21 -0700

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,38 +1,21 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
 
-from requests import get
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent, allow_redirects=False)
-
-    if response.status_code != 200:
-        print(f"Error: Received status code {response.status_code}")
-        return 0
-
+    """Return the total number of subscribers on a given subreddit."""
     try:
-        results = response.json()
-        return results['data']['subscribers']
-    except (ValueError, KeyError) as e:
-        print(f"Error: {e}")
-        return 0
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
-
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0\
+            (by /u/Large_Alternative_30)"
+        }
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 404:
+            return 0
+        results = response.json().get("data")
+        return results.get("subscribers")
+    except (Exception):
+        print('Not Found')
+        return (0)
