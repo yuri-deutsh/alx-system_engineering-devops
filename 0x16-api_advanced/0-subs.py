56a43b12a923f2fe5eba96e7c71af47ee15302f7
nabil mkadmi <nabilmkadmi@gmail.com>
2024-06-04 16:33:27 -0700

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given subreddit
+Queries the Reddit API and returns the number
+of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given,
+the function should return 0.
 """
-
-from requests import get
-
+import requests
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
+    """Returns number of total subscribers"""
+    url = f"https://api.reddit.com/r/{subreddit}/about"
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if subreddit is None or not isinstance(subreddit, str):
+    if response.status_code != 200:
         return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
 
     try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+        data = response.json().get('data', {})
+        return data.get('subscribers', 0)
+    except (ValueError, KeyError):
         return 0
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage: {} <subreddit>".format(sys.argv[0]))
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
